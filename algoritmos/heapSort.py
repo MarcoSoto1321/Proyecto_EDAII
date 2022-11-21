fb680f9ee7438096959d6532ddf7fe9abaef6cfa
Soreck <72623307+MarcoSoto1321@users.noreply.github.com>
2022-11-20 20:38:55 -0600

    agregando funciones de ordenamiento

--- algoritmos/heapSort.py
+++ algoritmos/heapSort.py
@@ -2,22 +2,42 @@ class heapsort:
     def __init__(self) -> None:
         pass
 
-    def heapify(self, arr, N, i):
+    def heapify_diasParaEntregar(self, arr, N, i):
         largest = i  
         l = 2 * i + 1     
         r = 2 * i + 2    
-        if l < N and arr[largest].dato < arr[l].dato:
+        if l < N and arr[largest].diasParaEntregar < arr[l].diasParaEntregar:
             largest = l
-        if r < N and arr[largest].dato < arr[r].dato:
+        if r < N and arr[largest].diasParaEntregar < arr[r].diasParaEntregar:
             largest = r
         if largest != i:
-            arr[i].dato, arr[largest].dato = arr[largest].dato, arr[i].dato  # swap
-            self.heapify(arr, N, largest)
+            arr[i].diasParaEntregar, arr[largest].diasParaEntregar = arr[largest].diasParaEntregar, arr[i].diasParaEntregar  # swap
+            self.heapify_diasParaEntregar(arr, N, largest)
+
+    def heapSort_diasParaEntregar(self, arr):
+        N = len(arr)
+        for i in range(N//2 - 1, -1, -1):
+            self.heapify_diasParaEntregar(arr, N, i)
+        for i in range(N-1, 0, -1):
+            arr[i].diasParaEntregar, arr[0].diasParaEntregar = arr[0].diasParaEntregar, arr[i].diasParaEntregar  # swap
+            self.heapify_diasParaEntregar(arr, i, 0)
+
+    def heapify_urgencia(self, arr, N, i):
+        largest = i  
+        l = 2 * i + 1     
+        r = 2 * i + 2    
+        if l < N and arr[largest].urgencia < arr[l].urgencia:
+            largest = l
+        if r < N and arr[largest].urgencia < arr[r].urgencia:
+            largest = r
+        if largest != i:
+            arr[i].urgencia, arr[largest].urgencia = arr[largest].urgencia, arr[i].urgencia  # swap
+            self.heapify_urgencia(arr, N, largest)
 
     def heapSort(self, arr):
         N = len(arr)
         for i in range(N//2 - 1, -1, -1):
-            self.heapify(arr, N, i)
+            self.heapify_urgencia(arr, N, i)
         for i in range(N-1, 0, -1):
-            arr[i].dato, arr[0].dato = arr[0].dato, arr[i].dato  # swap
-            self.heapify(arr, i, 0)
+            arr[i].urgencia, arr[0].urgencia = arr[0].urgencia, arr[i].urgencia  # swap
+            self.heapify_urgencia(arr, i, 0)
