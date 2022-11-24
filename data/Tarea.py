bbf8c77f167168f6ed74adfb4ec06247185c9bc5
Soreck <72623307+MarcoSoto1321@users.noreply.github.com>
2022-11-24 16:42:53 -0600

    cambiando a algoritmos

--- data/Tarea.py
+++
@@ -1,9 +0,0 @@
-class Tarea:
-    def __init__(self,nombre,descripcion,urgencia,tiempo,enCasa,diasParaEntregar):
-        self.nombre = nombre
-        self.descripcion =descripcion 
-        self.urgencia = urgencia
-        self.tiempo = tiempo
-        self.enCasa = enCasa
-        self.diasParaEntregar = diasParaEntregar
-        
