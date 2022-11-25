0de84acf7d602d7ae97f7f32e48326a765390060
Soreck <72623307+MarcoSoto1321@users.noreply.github.com>
2022-11-24 18:38:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,28 @@ def menu():
     print("1. ingresar tarea\n")
     print("2. Modificar tarea\n")
     print("3. Imprimir tareas\n")
-    print("4. Crear ruta de tareas\n")
-    print("Crear lista de compras\n")
+    print("4. ordenar tareas\n")
+    print("5. buscar tareas\n")
+    print("6. Crear ruta de tareas\n")
+    print("7. Crear lista de compras\n")
+  
+def seleccciones(select):
+    if select == 1:
+        print("")
+    elif select == 2:
+        print("")
+    elif select == 3:
+        print("")
+    elif select == 4:
+        print("")
+    elif select == 5:
+        print("")
+    elif select == 6:
+        print("")
+    elif select == 7:
+        print("")
+    else:
+        print("")
 
 if __name__ = "__main__":
     menu()
