13528e3f719ffe2899551d8aecc0380377550c76
Soreck <72623307+MarcoSoto1321@users.noreply.github.com>
2022-11-24 18:15:41 -0600

    agregando posiboles funciones

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+def listaCompras():
 
+def ordenar():
+    #orden por urgencia y por dias para entregar
+def buscar():
+    #busqueda por nombre, fecha e importancia
+def crearRuta():
+
+def ingresarTarea():
+    #los nombres no deben estar repetidos
+    #determinar valores predefinidos en caso de que no se ingrese un valor
+def modificarTarea():
+
+def imprimirTareas():
+
+def menu():
+    print("(^o^)/==================(^o^)/\n")
+    print("1. ingresar tarea\n")
+    print("2. Modificar tarea\n")
+    print("3. Imprimir tareas\n")
+    print("4. Crear ruta de tareas\n")
+    print("Crear lista de compras\n")
+
+if __name__ = "__main__":
+    menu()
