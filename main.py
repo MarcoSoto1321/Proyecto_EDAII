cb19ae68ecb17b769268603ae5baff592388840a
Soreck <72623307+MarcoSoto1321@users.noreply.github.com>
2022-11-24 20:04:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,14 @@
+from algoritmos import Funciones
+from algoritmos import heapSort
+from algoritmos import Busqueda
 
-def selecccionTareas(select):
-    if select == 1:
-        print("")
-    elif select == 2:
-        print("")
-    elif select == 3:
-        print("")
-    elif select == 4:
-        print("")
-    elif select == 5:
-        print("")
-    elif select == 6:
-        print("")
-    else:
-        print("")
 
-def selecccionCompras(select):
-    if select == 1:
-        print("")
-    elif select == 2:
-        print("")
-    elif select == 3:
-        print("")
-    else:
-        print("")
-
+ordenar =  heapSort()
 if __name__ == "__main__":
-    select = 0
     print("1. Tareas")
     print("2. Compras")
-    select = input("Ingresa la opcion")
+    select = int(input("Ingresa la opcion\n"))
+
     if select == 1:
 
         print("(^o^)/==================(^o^)/\n")
@@ -40,8 +19,35 @@ if __name__ == "__main__":
         print("5. buscar tareas\n")
         print("6. Crear ruta de tareas\n")
 
-        select = input("Ingresa la opcion")
-        selecccionTareas(select)
+        select = input("Ingresa la opcion\n")
+        if select == 1:#agregar tareas
+            print("")
+        elif select == 2:#modificar tareas
+            lista = []
+            nombre = ""
+            Funciones.modificar_tarea(lista, nombre)
+
+        elif select == 3:#imprimir tareas
+            print("")
+
+        elif select == 4:#ordenar tareas
+            lista = []
+            print("ordenar por: \n 1. dias para entregar \n 2. Prioridad")
+            opcion = int(input("Ingresa la opcion\n"))
+            if opcion == 1:
+                ordenar.heapSort_diasParaEntregar(lista)
+            elif opcion == 2:
+                ordenar.heapSort(lista)
+            else:
+                print("Ingresa una opcion valida")
+
+        elif select == 5:#buscar tareas
+            print("")
+        elif select == 6:#ruta de tareas
+            lista = []
+            Funciones.crear_grafo(lista)
+        else:
+            print("")
         
     if select == 2:
 
@@ -51,4 +57,12 @@ if __name__ == "__main__":
         print("3. Precio productos\n")
 
         select = input("Ingresa la opcion")
-        selecccionCompras(select)
+        if select == 1:#ingresar productos
+            print("")
+        elif select == 2:#imprimir productos
+            print("")
+        elif select == 3:#precioproductos
+            print("")
+        else:
+            print("")
+
