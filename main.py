64a1f749e096fd94d6f3f86d783588f9c35c72ad
Soreck <72623307+MarcoSoto1321@users.noreply.github.com>
2022-11-24 18:47:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,4 @@
-from algoritmos import heapSort
 
-def listaCompras():
-
-def ordenar():
-    #orden por urgencia y por dias para entregar
-    sort = heapSort()
-    #orden por urgencia y por dias para entregar
-    if select == 1:
-        sort.heapSort_diasParaEntregar(lista)
-    if select == 2:
-        sort.heapSort_urgencua(lista)
-    else:
-        print("Opcion incorrecta, orden por defecto 'urgencia'")
-        return ordenar(2, lista)
-def buscar():
-    #busqueda por nombre, fecha e importancia
-def crearRuta():
-
-def ingresarTarea():
-    #los nombres no deben estar repetidos
-    #determinar valores predefinidos en caso de que no se ingrese un valor
-def modificarTarea():
-
-def imprimirTareas():
 
 def menu():
     print("(^o^)/==================(^o^)/\n")
