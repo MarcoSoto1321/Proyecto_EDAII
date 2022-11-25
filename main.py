447f3a3f2f017ed74e4e42c76de8e20db3e5772e
Soreck <72623307+MarcoSoto1321@users.noreply.github.com>
2022-11-24 19:08:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 
-
-def menu():
-    print("(^o^)/==================(^o^)/\n")
-    print("1. ingresar tarea\n")
-    print("2. Modificar tarea\n")
-    print("3. Imprimir tareas\n")
-    print("4. ordenar tareas\n")
-    print("5. buscar tareas\n")
-    print("6. Crear ruta de tareas\n")
-    print("7. Crear lista de compras\n")
-  
-def seleccciones(select):
+def selecccionTareas(select):
     if select == 1:
         print("")
     elif select == 2:
@@ -23,10 +12,43 @@ def seleccciones(select):
         print("")
     elif select == 6:
         print("")
-    elif select == 7:
+    else:
+        print("")
+
+def selecccionCompras(select):
+    if select == 1:
+        print("")
+    elif select == 2:
+        print("")
+    elif select == 3:
         print("")
     else:
         print("")
 
-if __name__ = "__main__":
-    menu()
+if __name__ == "__main__":
+    select = 0
+    print("1. Tareas")
+    print("2. Compras")
+    select = input("Ingresa la opcion")
+    if select == 1:
+
+        print("(^o^)/==================(^o^)/\n")
+        print("1. ingresar tarea\n")
+        print("2. Modificar tarea\n")
+        print("3. Imprimir tareas\n")
+        print("4. ordenar tareas\n")
+        print("5. buscar tareas\n")
+        print("6. Crear ruta de tareas\n")
+
+        select = input("Ingresa la opcion")
+        selecccionTareas(select)
+        
+    if select == 2:
+
+        print("(^o^)/==================(^o^)/\n")
+        print("1. ingresar productos\n")
+        print("2. Imprimir productos\n")
+        print("3. Precio productos\n")
+
+        select = input("Ingresa la opcion")
+        selecccionCompras(select)
